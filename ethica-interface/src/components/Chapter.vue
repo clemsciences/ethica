17f45927eaa4ce5cfac8e955d33476a44c30d8a7
Clément Besnier <clemsciences@aol.com>
2021-02-10 00:32:37 +0100

    Pars

--- ethica-interface/src/components/Chapter.vue
+++ ethica-interface/src/components/Chapter.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="accordion" role="tablist">
-      <b-button class="toggle-button outline-primary" v-b-toggle="chapter.id" role="tab" @click="visible = !visible">Capita {{ chapter.id + 1 }}</b-button>
+      <b-button class="toggle-button outline-primary" v-b-toggle="chapter.id" role="tab" @click="visible = !visible">Pars {{ chapter.id + 1 }}</b-button>
       <b-collapse accordion="my-accordion" role="tabpanel" v-model="visible">
         <text-entity :id="chapter.id" :entity="entity" v-for="entity in chapter.content" :key="entity.id" />
       </b-collapse>

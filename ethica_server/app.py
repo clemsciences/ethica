f8a1a6223cc0b48efe4c2d677eefbb7fc3eb8629
Clément Besnier <clemsciences@aol.com>
2021-04-28 19:52:55 +0200

    Cross origin

--- ethica_server/app.py
+++ ethica_server/app.py
@@ -2,12 +2,15 @@
 
 """
 
+from flask_cors import CORS
 
 from ethica_server import create_app
 from ethica_server.routes import ethica
 
 
 app = create_app(ethica)
+cors = CORS()
+cors.init_app(app)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5010)
--- ethica_server/server_app.py
+++ ethica_server/server_app.py
@@ -4,3 +4,5 @@ from ethica_server import create_prod_app
 from ethica_server.routes import ethica
 
 app = create_prod_app(ethica)
+cors = CORS()
+cors.init_app(app)
